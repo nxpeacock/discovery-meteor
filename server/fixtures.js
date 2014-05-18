ead81dcd8b32874ec515366aad17c7d09595575c
nxpeacock <nguyenxuancong@outlook.com>
2014-05-19 01:54:38 +0700

    initial commit

---
+++ server/fixtures.js
@@ -0,0 +1,28 @@
+if(Posts.find().count()===0){
+  Posts.insert({
+    "title" : "Thủ tướng yêu cầu không để xảy ra biểu tình trái pháp luật",
+    "author" : "Xuan Hoa",
+    "url" : "http://vnexpress.net/tin-tuc/thoi-su/thu-tuong-yeu-cau-khong-de-xay-ra-bieu-tinh-trai-phap-luat-2992178.html"
+  });
+  Posts.insert({
+    "title" : "Những đối tượng lợi dụng lòng yêu nước sẽ sớm bị xét xử",
+    "author" : "poll",
+    "url" : "http://dantri.com.vn/the-gioi/nhung-doi-tuong-loi-dung-long-yeu-nuoc-se-som-bi-xet-xu-875991.htm"
+  });
+  Posts.insert({
+    "title" : "Phó Thủ tướng Vũ Đức Đam: Chủ quyền đất nước là thiêng liêng",
+    "author" : "Xuan Hoa",
+    "url" : "http://dantri.com.vn/su-kien/pho-thu-tuong-vu-duc-dam-chu-quyen-dat-nuoc-la-thieng-lieng-876065.htm"
+  });
+  Posts.insert({
+    "title" : "Công nhân Trung Quốc cảm kích tấm lòng của y bác sỹ Việt Nam",
+    "author" : "Xuan Hoa",
+    "url" : "http://dantri.com.vn/su-kien/cong-nhan-trung-quoc-cam-kich-tam-long-cua-y-bac-sy-viet-nam-876006.htm"
+  });
+  Posts.insert({
+    "title" : "Không để tái diễn Bình Dương, Vũng Áng",
+    "author" : "Xuan Hoa",
+    "url" : "http://vietnamnet.vn/vn/chinh-tri/176005/khong-de-tai-dien-binh-duong--vung-ang.html"
+  });
+
+}
