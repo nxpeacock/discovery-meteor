b3cadc93c551a9998bd55fd8edc67da17729627e
nxpeacock <nguyenxuancong@outlook.com>
2014-05-19 17:53:16 +0700

    9-3

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,12 +1,20 @@
 Template.postSubmit.events({
-    'submit form': function(e) {
+    'submit form': function (e) {
         e.preventDefault();
         var post = {
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val(),
             message: $(e.target).find('[name=message]').val()
         }
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+        Meteor.call('post', post, function(error, id) {
+            if (error) {
+// display the error to the user
+                throwError(error.reason);
+                if (error.error === 302)
+                    Router.go('postPage', {_id: error.details})
+            } else {
+                Router.go('postPage', {_id: id});
+            }
+        });
     }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,8 +1,65 @@
 Posts = new Meteor.Collection('posts');
 
+/*
+ Posts.allow({
+ insert: function(userId, doc) {
+ // only allow posting if you are logged in
+ return !! userId;
+ }
+ });*/
+
 Posts.allow({
-    insert: function(userId, doc) {
-// only allow posting if you are logged in
-        return !! userId;
+    update : ownsDocument,
+    remove : ownsDocument
+});
+
+Posts.deny({
+    update: function(userId, post, fieldNames) {
+// may only edit the following two fields:
+        return (_.without(fieldNames, 'url', 'title').length > 0);
     }
 });
+
+Meteor.methods({
+    post: function (postAttributes) {
+        var user = Meteor.user(),
+            postWithSameLink = Posts.findOne({url: postAttributes.url});
+        if (!user) {
+            throw new Meteor.Error(401, "You need to login to post new stories");
+        }
+
+        // ensure the post has a title
+        if (!postAttributes.title)
+            throw new Meteor.Error(422, 'Please fill in a headline');
+
+        // check that there are no previous posts with the same link
+        if (postAttributes.url && postWithSameLink) {
+            throw new Meteor.Error(302,
+                'This link has already been posted',
+                postWithSameLink._id);
+        }
+
+// pick out the whitelisted keys
+        var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+            title: postAttributes.title ,
+            userId: user._id,
+            author: user.username,
+            submitted: new Date().getTime()
+        });
+/*
+
+        // wait for 5 seconds
+        if (! this.isSimulation) {
+            var Future = Npm.require('fibers/future');
+            var future = new Future();
+            Meteor.setTimeout(function() {
+                future.return();
+            }, 5 * 1000);
+            future.wait();
+        }
+*/
+
+        var postId = Posts.insert(post);
+        return postId;
+    }
+})
